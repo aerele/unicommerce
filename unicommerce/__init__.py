b02e5a51ac16949af88b0023fdff0c9ec3d27c83
Bhavathariniya <bhavathariniya13@gmail.com>
2026-07-15 07:00:07 +0530

    feat(desk): add Workspace, Sidebar, Desktop Icon, and apps-screen entry

--- unicommerce/__init__.py
+++ unicommerce/__init__.py
@@ -1,1 +1,15 @@
 __version__ = "0.0.1"
+
+import frappe
+
+
+def check_app_permission():
+	from frappe.utils.user import is_website_user
+
+	if frappe.session.user == "Administrator":
+		return True
+
+	if is_website_user():
+		return False
+
+	return True
---
+++ unicommerce/config/desktop.py
@@ -0,0 +1,13 @@
+from frappe import _
+
+
+def get_data():
+	return [
+		{
+			"module_name": "unicommerce",
+			"color": "blue",
+			"icon": "octicon octicon-package",
+			"type": "module",
+			"label": _("Unicommerce"),
+		}
+	]
